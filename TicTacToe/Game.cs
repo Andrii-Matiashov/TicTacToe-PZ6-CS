b8dc70381808de5a49e3571cb8f6582493169e31
Andrii-Matiashov <andrii.matiashov@nure.ua>
2023-06-03 11:31:58 +0100

    Добавьте файлы проекта.

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ6
+{
+    public enum PlayerQueueValue 
+    {
+        CROSS = -1,
+        ZERO = 0
+    }
+
+    internal class Game
+    {
+        private sbyte[][] data;
+        private const int size = 3;
+        private bool gameover;
+        private PlayerQueueValue queueValue;
+        private sbyte _value;
+
+        public bool GameOver { get { return gameover; } set { gameover = value; } }
+        public sbyte[][] Data { get { return data; }}
+        public sbyte Value { get { return _value; } set { _value = value; } }
+        public Game()
+        {
+            data = new sbyte[size][];
+            for (int i = 0; i < size; ++i)
+            {
+                data[i] = new sbyte[size];
+            }
+            gameover = false;
+            queueValue = PlayerQueueValue.CROSS;
+            _value = 1;
+        }
+
+        public void RestartGame()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data[i].Length; ++j)
+                {
+                    data[i][j] = 0;
+                }
+            }
+            gameover = false;
+            queueValue = PlayerQueueValue.CROSS;
+            _value = 1;
+        }
+
+
+        public void SwitchQueue()
+        {
+            queueValue = ~queueValue;
+
+            switch (queueValue)
+            {
+                case PlayerQueueValue.CROSS:
+                    _value = 1;
+                    break;
+                case PlayerQueueValue.ZERO:
+                    _value = -1;
+                    break;
+                default:
+                    _value = 0;
+                    break;
+            }
+        }
+
+        public bool GameOverCheck()
+        {
+            bool flag = false;
+
+            for (int i = 0; i < size; ++i)
+            {
+                flag = CheckArray(data[i][0], data[i][1], data[i][2])
+                    || CheckArray(data[0][i], data[1][i], data[2][i]);
+                if (flag) return flag;
+            }
+
+            flag = CheckArray(data[0][0], data[1][1], data[2][2])
+                || CheckArray(data[0][2], data[1][1], data[2][0]);
+          
+            return flag;
+        }
+
+        public bool SetValue(int i, int j)
+        {
+            if (i < 0 || j < 0)
+                return false;
+
+            if (i >= size || j >= size)
+                return false;
+
+            if (data[i][j] != 0)
+                return false;
+
+            data[i][j] = _value;
+            return true;
+        }
+
+        public bool CheckArray(params sbyte[] args)
+        {
+
+            bool flag = true;
+            for (int i = 0; i < args.Length; ++i)
+            {
+                if (args[0] != args[i] || args[i] == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+}
---
+++ TicTacToe/MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading;
+
+using PZ6;
+using System.Threading.Tasks;
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Game game;
+        private int oWins;
+        private int xWins;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Stats.Text = $"Wins:\n x: {xWins}\no: {oWins}";
+            xWins = 0;
+            oWins = 0;
+            game = new Game();
+        }
+
+        private void Cell_Click(object sender, RoutedEventArgs args)
+        {
+            Button button = sender as Button;
+
+            int[] result = ParseXY(button.Name.ToString());
+
+            if (game.GameOver) {
+                Info.Text = $"Game over! Winner = {GetSymbol(game.Value)}";
+                if(game.Value == 1)
+                {
+                    xWins++;
+                }
+                else
+                {
+                    oWins++;
+                }
+                return;
+            }
+
+            if (!game.SetValue(result[0], result[1]))
+            {
+                return;
+            }
+
+            Render();
+
+            if (game.GameOver = game.GameOverCheck())
+            {
+                Info.Text = $"Game over! Winner = {GetSymbol(game.Value)}";
+                if (game.Value == 1)
+                {
+                    xWins++;
+                }
+                else
+                {
+                    oWins++;
+                }
+                return;
+            }
+            
+            game.SwitchQueue();
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs args)
+        {
+            Info.Text = "";
+            game.RestartGame();
+            Render();
+        }
+
+        private void Render()
+        {
+            cell00.Content = $"{GetSymbol(game.Data[0][0])}";
+            cell01.Content = $"{GetSymbol(game.Data[0][1])}";
+            cell02.Content = $"{GetSymbol(game.Data[0][2])}";
+            cell10.Content = $"{GetSymbol(game.Data[1][0])}";
+            cell11.Content = $"{GetSymbol(game.Data[1][1])}";
+            cell12.Content = $"{GetSymbol(game.Data[1][2])}";
+            cell20.Content = $"{GetSymbol(game.Data[2][0])}";
+            cell21.Content = $"{GetSymbol(game.Data[2][1])}";
+            cell22.Content = $"{GetSymbol(game.Data[2][2])}";
+            Stats.Text = $"Wins:\n x: {xWins}\no: {oWins}";
+        }
+
+        private int[] ParseXY(string button_name)
+        {
+
+            int[] result = new int[2];
+            string values = button_name.Replace("cell", "");
+            result[0] = int.Parse(values[0].ToString());
+            result[1] = int.Parse(values[1].ToString());
+            return result;
+        }
+
+        private char GetSymbol(sbyte value) {
+            switch (value) {
+                
+                case 1:
+                    return 'x';
+                case -1:
+                    return 'o';
+                default:
+                    return (char)0;
+            }
+        }
+    }
+}
